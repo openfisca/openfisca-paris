8a01af6bff5572ccd654b267aa5d8cad6be92df2
mendy <kinta.mendy@implicit.fr>
2017-08-31 15:11:05 +0200

    Contraint la version OpenFisca-France

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 15, < 16',
-        'OpenFisca-France >= 18, < 19'
+        'OpenFisca-France >= 18, < 18.7'
     ],
     extras_require = {
         'test': 'nose'
