56741b91355991143752803f466704ffbfe74d94
Thomas Guillet <guillet.thomas@gmail.com>
2017-09-04 11:44:40 +0200

    Migrate XML parameters to YAML

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.0.3",
+    version="2.0.4",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 15, < 16',
-        'OpenFisca-France >= 18, < 18.7'
+        'OpenFisca-Core >= 17, < 18',
+        'OpenFisca-France >= 18.9, < 19'
     ],
     extras_require = {
         'test': 'nose'
