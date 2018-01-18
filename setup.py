5cd26480a0fa160594809298fdff0861a86c59aa
Thomas Guillet <guillet.thomas@gmail.com>
2018-01-18 17:46:01 +0100

    Upgrade requirement versions

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.0.5",
+    version="2.1.0",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 20, < 21',
-        'OpenFisca-France >= 18.11, < 20'
+        'OpenFisca-Core >= 21, < 22',
+        'OpenFisca-France >= 20, < 20.1'
     ],
     extras_require = {
         'test': 'nose'
