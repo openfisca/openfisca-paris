75cfc5d4d71fcb3cb4a4bfbc00be66f495a3258b
Thomas Guillet <guillet.thomas@gmail.com>
2019-02-06 14:03:26 +0100

    Distingue l'éligibilité financière des autres critères dans Paris Logement Familles Mono

--- openfisca_paris/paris_logement_fam_mono.py
+++ openfisca_paris/paris_logement_fam_mono.py
@@ -6,7 +6,31 @@ from numpy import (maximum as max_,  logical_not as not_,  absolute as abs_,  mi
 from openfisca_france.model.base import *  # noqa analysis:ignore
 
 
-# Paris logement familles monoparentales
+class paris_logement_est_monoparentale(Variable):
+    value_type = bool
+    label = u"Famille monoparentale au sens de Paris logement familles monoparentales"
+    entity = Famille
+    definition_period = MONTH
+
+    def formula(famille, period, legislation):
+        last_month = period.last_month
+
+        parent_solo = not_(famille('en_couple', period))
+        nb_enfants = famille('paris_nb_enfants', period)
+        parisien = famille('parisien', period)
+
+        statut_occupation_logement = famille.demandeur.menage('statut_occupation_logement', period)
+        statut_occupation_plfm = (
+            (statut_occupation_logement == TypesStatutOccupationLogement.primo_accedant) +
+            (statut_occupation_logement == TypesStatutOccupationLogement.proprietaire) +
+            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_hlm) +
+            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_vide) +
+            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_meuble) +
+            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_foyer)
+        )
+
+        return  parent_solo * (nb_enfants >= 1) * (nb_enfants < 4) * parisien * statut_occupation_plfm
+
 
 class paris_logement_plfm(Variable):
     value_type = float
@@ -22,24 +46,13 @@ class paris_logement_plfm(Variable):
         aide_1er_plafond_plfm = legislation(period).paris.plfm.aide_1er_plafond_plfm
         aide_2eme_plafond_plfm = legislation(period).paris.plfm.aide_2eme_plafond_plfm
 
-        parent_solo = not_(famille('en_couple', period))
-        nb_enfants = famille('paris_nb_enfants', period)
-        parisien = famille('parisien', period)
-        statut_occupation_logement = famille.demandeur.menage('statut_occupation_logement', period)
-        statut_occupation_plfm = (
-            (statut_occupation_logement == TypesStatutOccupationLogement.primo_accedant) +
-            (statut_occupation_logement == TypesStatutOccupationLogement.proprietaire) +
-            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_hlm) +
-            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_vide) +
-            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_meuble) +
-            (statut_occupation_logement == TypesStatutOccupationLogement.locataire_foyer)
-        )
-
         loyer = famille.demandeur.menage('loyer', period)
         charges_locatives = famille.demandeur.menage('charges_locatives', period)
         paris_base_ressources_commun = famille('paris_base_ressources_commun', last_month)
         aide_logement = famille('aide_logement', last_month)
         loyer_net = famille('paris_loyer_net', period)
+
+        est_monoparentale = famille('paris_logement_est_monoparentale', period)
 
         ressources_mensuelles_famille = paris_base_ressources_commun + aide_logement
 
@@ -48,7 +61,7 @@ class paris_logement_plfm(Variable):
 
         condition_plfm = where((montant_aide > loyer_net), (montant_aide - (montant_aide - loyer_net)), montant_aide)
 
-        result_montant = condition_plfm * parent_solo * (nb_enfants >= 1) * (nb_enfants < 4) * parisien * statut_occupation_plfm * ((loyer > 0) + (charges_locatives > 0))
+        result_montant = condition_plfm * ((loyer > 0) + (charges_locatives > 0)) * est_monoparentale
 
         result = where(result_montant > 0, result_montant, 0)
 
