d3393e5c23711bdfdefa42129791f1319513c655
Thomas Guillet <guillet.thomas@gmail.com>
2019-02-28 16:26:17 +0100

    Indique le paquet compatible avec CoreV26 et FranceV35

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 25, < 26',
-        'OpenFisca-France >= 32.2, < 35'
+        'OpenFisca-Core >= 25, < 27',
+        'OpenFisca-France >= 32.2, < 36'
     ],
     extras_require = {
         'test': 'nose'
