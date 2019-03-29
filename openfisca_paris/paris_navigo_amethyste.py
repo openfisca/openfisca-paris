5dc8ff8af1654f990776cf91eb4c5d540cf806cb
Thomas Guillet <guillet.thomas@gmail.com>
2019-03-29 09:22:02 +0100

    Subdivise Paris Emeraude Amethyste

--- openfisca_paris/paris_navigo_amethyste.py
+++ openfisca_paris/paris_navigo_amethyste.py
@@ -4,20 +4,6 @@ from numpy import (maximum as max_, logical_not as not_, absolute as abs_, minim
 from openfisca_france.model.base import *  # noqa analysis:ignore
 
 
-class paris_navigo_amethyste_eligibilite_personnelle(Variable):
-    value_type = bool
-    label = u"Éligibité des personnes au Pass Navigo Améthyste de Paris"
-    entity = Individu
-    definition_period = MONTH
-    reference = "Articles II.3.1.b et III.3.1.b du règlement municipal du CASVP"
-
-    def formula(individu, period):
-        return individu.famille('parisien', period) * (
-            + individu('paris_personnes_agees', period)
-            + individu('paris_personnes_handicap', period)
-            )
-
-
 class paris_navigo_amethyste_eligibilite_financiere(Variable):
     value_type = bool
     label = u"Éligibilité financière au Pass Navigo Améthyste de Paris"
@@ -35,13 +21,29 @@ class paris_navigo_amethyste_eligibilite_financiere(Variable):
         return elig + elig_renouvellement
 
 
-class paris_navigo_amethyste(Variable):
+class paris_pass_seniors(Variable):
     value_type = bool
-    label = u"Éligibilité au Pass Navigo Améthyste de Paris"
+    label = u"Éligibité au Pass Paris Seniors"
     entity = Individu
     definition_period = MONTH
 
     def formula(individu, period):
-        eligibilite_personnelle = individu('paris_navigo_amethyste_eligibilite_personnelle', period)
-        eligibilite_financiere = individu.foyer_fiscal('paris_navigo_amethyste_eligibilite_financiere', period)
-        return eligibilite_personnelle * eligibilite_financiere
+        return (
+            individu.famille('parisien', period)
+            * individu('paris_personnes_agees', period)
+            * individu.foyer_fiscal('paris_navigo_amethyste_eligibilite_financiere', period)
+        )
+
+
+class paris_pass_access(Variable):
+    value_type = bool
+    label = u"Éligibité au Pass Paris Access'"
+    entity = Individu
+    definition_period = MONTH
+
+    def formula(individu, period):
+        return (
+            individu.famille('parisien', period)
+            * individu('paris_personnes_handicap', period)
+            * individu.foyer_fiscal('paris_navigo_amethyste_eligibilite_financiere', period)
+        )
