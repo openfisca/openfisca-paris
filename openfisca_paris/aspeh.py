045287b4ef6d0d813ed30f104fcd7df81a7fadcc
Thomas Guillet <guillet.thomas@gmail.com>
2019-05-07 18:21:16 +0200

    Refactor les formules de ASPEH
    * allocation de soutien aux parents d’enfants handicapés

--- openfisca_paris/aspeh.py
+++ openfisca_paris/aspeh.py
@@ -7,6 +7,18 @@ from openfisca_france.model.base import *  # noqa analysis:ignore
 
 # Allocation de soutien aux parents d’enfants handicapés
 
+class paris_logement_aspeh_base_ressources(Variable):
+    value_type = float
+    label = u"Base ressources pour l'Allocation de soutien aux parents d’enfants handicapés"
+    entity = Famille
+    definition_period = MONTH
+
+    def formula(famille, period, legislation):
+        base_ressources = famille('paris_base_ressources_couple', period.last_month)
+        clca = famille('paje_clca', period.last_month)
+        return base_ressources + clca
+
+
 class paris_logement_aspeh(Variable):
     value_type = float
     label = u"Le montant de l'Allocation de soutien aux parents d’enfants handicapés"
@@ -14,21 +26,11 @@ class paris_logement_aspeh(Variable):
     definition_period = MONTH
 
     def formula(famille, period, legislation):
-        last_month = period.last_month
-
         plafond_aspeh = legislation(period).paris.aspeh.plafond_aspeh
-        aide_aspeh = legislation(period).paris.aspeh.aide_aspeh
+        montant = legislation(period).paris.aspeh.aide_aspeh
 
         parisien = famille('parisien', period)
+        nb_enfant_handicape = famille('paris_nb_enfants_handicapes', period)
+        ressources = famille('paris_logement_aspeh_base_ressources', period)
 
-        enfant_handicape = famille.members('paris_enfant_handicape', period)
-        nb_enfant = famille.sum(enfant_handicape)
-
-        base_ressources = famille('paris_base_ressources_couple', last_month)
-        clca = famille('paje_clca', last_month)
-
-        ressources_mensuelles_famille = base_ressources + clca
-
-        result = select([ressources_mensuelles_famille <= plafond_aspeh],
-            [aide_aspeh]) * parisien
-        return result * nb_enfant
+        return parisien * (ressources <= plafond_aspeh) * nb_enfant_handicape * montant
