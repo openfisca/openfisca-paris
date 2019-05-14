bacf2424930b3efe47245894d805ee37f6e97fc0
ManonLger <manon.leger@ntymail.com>
2019-05-14 17:26:11 +0200

    Nouveau dossier pour les aides 'familles'

--- openfisca_paris/familles/paris_forfait_famille.py
+++ openfisca_paris/familles/paris_forfait_famille.py
@@ -5,10 +5,10 @@ from numpy import (maximum as max_, logical_not as not_, absolute as abs_, minim
 
 from openfisca_france.model.base import *  # noqa analysis:ignore
 
-# Paris forfait familles
-class paris_forfait_famille_elig(Variable):
+
+class paris_forfait_famille_eligibilite(Variable):
     value_type = bool
-    label = u"Eligibilité à Paris Forfait Famille"
+    label = u"Eligibilité à l'aide Paris Forfait Famille"
     entity = Famille
     definition_period = MONTH
 
@@ -30,20 +30,20 @@ class paris_forfait_famille_elig(Variable):
 
 class paris_forfait_famille(Variable):
     value_type = float
-    label = u"Famille qui est eligible à l'aide paris forfait famille "
+    label = u"Aide Paris Forfait Famille"
     entity = Famille
     definition_period = MONTH
 
     def formula(famille, period, legislation):
         last_month = period.last_month
 
-        premier_plafond = legislation(period).paris.paris_forfait_famille.premier_plafond
-        deuxieme_plafond = legislation(period).paris.paris_forfait_famille.deuxieme_plafond
-        aide_1er_plafond = legislation(period).paris.paris_forfait_famille.aide_1er_plafond
-        aide_2eme_plafond = legislation(period).paris.paris_forfait_famille.aide_2eme_plafond
+        premier_plafond = legislation(period).paris.familles.paris_forfait_famille.premier_plafond
+        deuxieme_plafond = legislation(period).paris.familles.paris_forfait_famille.deuxieme_plafond
+        aide_1er_plafond = legislation(period).paris.familles.paris_forfait_famille.aide_1er_plafond
+        aide_2eme_plafond = legislation(period).paris.familles.paris_forfait_famille.aide_2eme_plafond
 
         nb_enfants = famille('paris_nb_enfants', period)
-        elig = famille('paris_forfait_famille_elig', period)
+        elig = famille('paris_forfait_famille_eligibilite', period)
         ressources_mensuelles_famille = famille('paris_base_ressources_couple', last_month)
         montant_aide = select([(ressources_mensuelles_famille <= premier_plafond),
             (ressources_mensuelles_famille <= deuxieme_plafond)], [aide_1er_plafond, aide_2eme_plafond])
