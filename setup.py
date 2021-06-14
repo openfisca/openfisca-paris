864bba53a5aedb47563eccc94570cddc0f2163e2
Thomas Guillet <thomas@palya.fr>
2021-06-14 17:11:00 +0200

    Indique la compabilité avec France 53 (#114)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.2.1",
+    version="2.2.2",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 25.3, < 36',
-        'OpenFisca-France >= 34.8.0, < 53'
+        'OpenFisca-France >= 34.8.0, < 54'
     ],
     extras_require = {
         'test': 'nose'
