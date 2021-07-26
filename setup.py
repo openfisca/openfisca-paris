d79ad6b2ace738776403806946c4eae05b98bc8f
Thomas Guillet <thomas@palya.fr>
2021-07-26 17:58:58 +0200

    Ajuste les formules aux renommages faits dans France

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.2.3",
+    version="2.2.4",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 25.3, < 36',
-        'OpenFisca-France >= 34.8.0, < 55'
+        'OpenFisca-Core >= 35.2.0, < 36',
+        'OpenFisca-France >= 58, < 61'
     ],
     extras_require = {
-        'test': 'nose'
+        'test': [
+            'nose',
+            'pytest >= 5.4.2'
+            ]
     },
     classifiers=[
         "Programming Language :: Python",
