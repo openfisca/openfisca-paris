dfb5c1e8e083e932dea757e30ca9bfa6e1cf7087
Kout95 <65901733+Kout95@users.noreply.github.com>
2021-09-09 11:08:55 +0200

    Met à jour la version d'openfisca-france < 75 (#120)
    * Met à jour la version d'openfisca-france
    
    * Monte la version d'openfisca france en version 74

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.2.4",
+    version="2.2.5",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 35.2.0, < 36',
-        'OpenFisca-France >= 61, < 62'
+        'OpenFisca-France >= 61, < 75'
     ],
     extras_require = {
         'test': [
