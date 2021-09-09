518a03864bad2aecad69531b5a320940636f74dd
Kout95 <65901733+Kout95@users.noreply.github.com>
2021-09-09 11:03:56 +0200

    Monte la version d'openfisca france en version 74

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 35.2.0, < 36',
-        'OpenFisca-France >= 61, < 74'
+        'OpenFisca-France >= 61, < 75'
     ],
     extras_require = {
         'test': [
