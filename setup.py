1ae485e79d5a4faae7686ba5f753cf0ba79bdcff
kout95 <antoine.bernier@outlook.fr>
2021-10-29 11:09:54 +0200

    Met à jour la version openfisca

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Openfisca-Paris",
-    version="2.4.0",
+    version="2.4.1",
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 35.2.0, < 36',
-        'OpenFisca-France >= 61, < 80'
+        'OpenFisca-France >= 61, < 81'
     ],
     extras_require = {
         'test': [
