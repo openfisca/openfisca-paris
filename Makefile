4a209828a18ca4d54642decd7dc89ea6280550f8
kout95 <antoine.bernier@outlook.fr>
2022-01-19 10:10:13 +0100

    Met à jour le setup

--- Makefile
+++ Makefile
@@ -8,6 +8,25 @@ clean:
 	rm -rf build dist
 	find . -name '*.pyc' -exec rm \{\} \;
 
-install:
-	pip install --upgrade pip wheel
-	pip install --editable .[test] --upgrade
+deps:
+	pip install --upgrade pip build twine
+
+install-test:
+	pip install --editable ".[test]"
+
+install: deps
+	@# Install OpenFisca-France-Local for development.
+	@# `make install` installs the editable version of OpenFisca-France-Local.
+	@# This allows contributors to test as they code.
+	pip install --editable . --upgrade
+
+build: clean deps
+	@# Install OpenFisca-France-Local for deployment and publishing.
+	@# `make build` allows us to be be sure tests are run against the packaged version
+	@# of OpenFisca-France-Local, the same we put in the hands of users and reusers.
+	python -m build
+	pip uninstall --yes openfisca-france-local
+	find dist -name "*.whl" -exec pip install {} \;
+
+test:
+	openfisca test tests --country-package openfisca_france --extension openfisca_paris
--- setup.py
+++ setup.py
@@ -4,9 +4,21 @@ from setuptools import setup, find_packages
 setup(
     name="Openfisca-Paris",
     version="3.1.0",
+    author="OpenFisca Team",
+    author_email="contact@openfisca.fr",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
+    keywords="benefit france paris microsimulation social tax",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    author="Mairie de Paris, Incubateur de Services Numériques (SGMAP)",
+
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -19,9 +31,4 @@ setup(
             'pytest >= 5.4.2'
             ]
     },
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7",
-    ]
 )
