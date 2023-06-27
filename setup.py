5d2c1ffda1d0bedb1c2efc44ab5bf1fb3861bf89
Allan-CodeWorks <101192539+Allan-CodeWorks@users.noreply.github.com>
2023-06-27 09:58:06 +0200

    Met à jour python et toutes les dépendances (#145)
    # Description
    ## Breaking changes
    - Met à jour la version minimale de `python` de `3.7`  vers python `3.9`
    - Met à jour la version minimale d'`openfisca-core` de `35.2.0` vers `40.0.1`
    - Met à jour la version minimale d'`openfisca-france`de `102`  vers `149.1.1`

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="Openfisca-Paris",
-    version="3.9.0",
+    version="4.0.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers=[
@@ -11,19 +11,20 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     description="Plugin OpenFisca pour les aides sociales de la mairie de Paris",
     keywords="benefit france paris microsimulation social tax",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
 
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 35.2.0, < 36',
-        'OpenFisca-France >= 102, < 148',
+        'OpenFisca-Core >= 40.0.1, < 41',
+        'OpenFisca-France >= 149.1.1, < 150',
     ],
     extras_require={
         'test': [
