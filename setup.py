47cba7998de1fec72d9bfe639ea5793dcce8250b
Allan-CodeWorks <101192539+Allan-CodeWorks@users.noreply.github.com>
2023-06-27 14:33:18 +0200

    Met à jours le workflow CI avec les dernières versions des actions github (#147)
    # Description :
    `cache@v2` -> `cache@v3` 
    `setup-python@v2` -> `setup-python@v4`
    `checkout@v2` -> `checkout@v3`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="Openfisca-Paris",
-    version="4.0.1",
+    version="4.0.2",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers=[
